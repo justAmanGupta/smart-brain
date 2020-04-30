fd8e6a3a7650697993c98ea22bd0c5954f0967f4
Aman Gupta <50948920+justAmanGupta@users.noreply.github.com>
2020-04-30 16:40:22 +0530

    updating message

---
+++ precache-manifest.80ae7aa11f9340753aba5fa90690c38a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4375dc096650f96d3c0cae301b6fec44",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "f5e4e513e885b5783a00",
+    "url": "/smart-brain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "fcb8bfd189e3bcd793c3",
+    "url": "/smart-brain/static/css/main.e4ade47a.chunk.css"
+  },
+  {
+    "revision": "f5e4e513e885b5783a00",
+    "url": "/smart-brain/static/js/2.fc9437fc.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smart-brain/static/js/2.fc9437fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb8bfd189e3bcd793c3",
+    "url": "/smart-brain/static/js/main.67d14a4c.chunk.js"
+  },
+  {
+    "revision": "5631173ee026feeb2beb",
+    "url": "/smart-brain/static/js/runtime-main.2f2f904b.js"
+  },
+  {
+    "revision": "799ab00be80c3530e0a4a3d67dee4a28",
+    "url": "/smart-brain/static/media/brain.799ab00b.png"
+  }
+]);
